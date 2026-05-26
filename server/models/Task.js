8e16db5d24c378ee9a063c67d2882bec354499cf
Yamini Nukathoti <yamininukathoti@gmail.com>
2026-05-26 12:03:24 +0530

    Added dashboard and authentication

---
+++ server/models/Task.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
+  dueDate: Date,
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project",
+  },
+});
+module.exports = mongoose.model("Task", taskSchema);
